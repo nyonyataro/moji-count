2750ba4cdf2f42ca1f5df1ae073dc4067e4d41bc
Taichi <nonatai0124@gmail.com>
2022-03-15 17:07:05 +0900

    完成

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect
-from app import delete
+from defs import delete, write, get_deviation
 from werkzeug.utils import secure_filename
 import os
 
@@ -12,9 +12,11 @@ def hello_world():
     if request.method == 'GET':
         return render_template('index.html', post = False)
     elif request.method =='POST':
+        #フォームに入力された時
         if request.form.get('txt'):
             txt = request.form.get('txt')
-        if request.files.get('file'):
+        #ファイルをアップロードされた時
+        elif request.files.get('file'):
             f = request.files.get('file')
             filepath = 'txt/' + secure_filename(f.filename)
             f.save(filepath)
@@ -23,5 +25,10 @@ def hello_world():
             txt = f_2.read()
             f_2.close()
             os.remove(filepath)
+        else:
+            return redirect('/')
         txt = delete(txt)
-        return render_template('index.html',length=len(txt), post = True)
+        length = len(txt)
+        write(length)
+        devidation = get_deviation(length)
+        return render_template('index.html',length=length, devidation = devidation, post = True)
